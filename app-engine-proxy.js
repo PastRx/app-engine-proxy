d45a54cba6b185b0cdb14f74bd7f48e901fe3ad7
Robert Cassidy <robertcassidy@new-host.home>
2019-03-25 15:33:29 -0400

    added Winston logger

--- app-engine-proxy.js
+++ app-engine-proxy.js
@@ -3,6 +3,26 @@ var http = require('http');
 var fs = require('fs');
 var url = require('url');
 var httpProxy = require('http-proxy');
+//Logging
+// winston 3 example.
+const winston = require('winston');
+
+// Imports the Google Cloud client library for Winston
+const {LoggingWinston} = require('@google-cloud/logging-winston');
+
+const loggingWinston = new LoggingWinston();
+
+// Create a Winston logger that streams to Stackdriver Logging
+// Logs will be written to: "projects/YOUR_PROJECT_ID/logs/winston_log"
+const logger = winston.createLogger({
+  level: 'info',
+  transports: [
+    new winston.transports.Console(),
+    // Add Stackdriver Logging
+    loggingWinston,
+  ],
+});
+
 
 var respond = function(status, msg, res){
   res.writeHead(status, {'content-type': 'text/plain'});
@@ -38,6 +58,7 @@ var server = http.createServer(function(req, res) {
   }
 
   if(!req.headers['x-target']) {
+    logger.info('required header "X-Target" not found');
     return respond(400, 'required header "X-Target" not found', res);
   }
 
@@ -47,6 +68,8 @@ var server = http.createServer(function(req, res) {
   var protocol = proxyURL.protocol;
   var agent = (protocol == 'https:' ? https.globalAgent:http.globalAgent);
   req.url = '';
+
+  logger.info('proxy request to: ' + target);
 
   proxy.web(req, res, {
     target: target,
