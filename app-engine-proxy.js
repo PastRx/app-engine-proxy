9f7f970f39c76c3ba41d6b4c3a1db3ac13ddb3b8
Mahesh <dmaheshwarareddy@gmail.com>
2022-02-03 22:38:17 -0500

    added https

--- app-engine-proxy.js
+++ app-engine-proxy.js
@@ -1,58 +1,92 @@
 var https = require('https');
+var http = require('http');
+var fs = require('fs');
 var url = require('url');
 var httpProxy = require('http-proxy');
-var httpsOptionsClient = require('./https-options')
+//Logging
+// winston 3 example.
+const winston = require('winston');
 
-/**
- * Starts the proxy server, requires httpsOptions
- */
-function startServer(httpsOptions) {
-  var respond = function (status, msg, res) {
-    res.writeHead(status, { 'content-type': 'text/plain' });
-    res.write(msg);
-    res.end();
-  }
+// Imports the Google Cloud client library for Winston
+const {LoggingWinston} = require('@google-cloud/logging-winston');
 
-  var proxy = httpProxy.createProxyServer({ ssl: httpsOptions });
+const loggingWinston = new LoggingWinston();
 
-  var server = https.createServer(httpsOptions, function (req, res) {
-    if (req.url == '/health-check') {
-      return respond(200, '', res);
-    }
+// Create a Winston logger that streams to Stackdriver Logging
+// Logs will be written to: "projects/YOUR_PROJECT_ID/logs/winston_log"
+const logger = winston.createLogger({
+  level: 'info',
+  transports: [
+    new winston.transports.Console(),
+    // Add Stackdriver Logging
+    loggingWinston,
+  ],
+});
 
-    if (!req.headers['x-target']) {
-      return respond(400, 'required header "X-Target" not found', res);
-    }
 
-    var target = req.headers['x-target'];
-    var targetUrl = url.parse(target);
-
-    // https://github.com/http-party/node-http-proxy/blob/master/lib/http-proxy/index.js#L65
-    // force client cert for client ssl cert authentication
-    if (req.headers['x-send-client-cert'] === 'true') {
-      targetUrl.key = httpsOptions.key;
-      targetUrl.cert = httpsOptions.cert;
-    }
-
-    var host = targetUrl.host;
-    var protocol = targetUrl.protocol;
-    var agent = (protocol == 'https:' ? https.globalAgent : http.globalAgent);
-    req.url = '';
-
-    proxy.web(req, res, {
-      target: targetUrl,
-      changeOrigin: true,
-      agent: agent,
-      headers: {
-        host: host
-      }
-    });
-
-  });
-
-  server.listen(443);
+var respond = function(status, msg, res){
+  res.writeHead(status, {'content-type': 'text/plain'});
+  res.write(msg);
+  res.end();
 }
 
-httpsOptionsClient.buildFromSecretManager().then(httpsOptions => {
-  startServer(httpsOptions);
+// //only used for HTTPS
+// var getHTTPSOptions = function() {
+//   return JSON.parse(fs.readFileSync('config.json', 'utf8')).keyCert;
+// }
+// var httpsOptions = {
+//   key: fs.readFileSync(getHTTPSOptions().key),
+//   cert: fs.readFileSync(getHTTPSOptions().cert),
+//   ca: fs.readFileSync(getHTTPSOptions().ca)
+// }
+
+
+var proxy = httpProxy.createProxyServer({});
+
+proxy.on('proxyReq', function (proxyReq, req, res) {
 });
+
+proxy.on('proxyRes', function (proxyRes, req, res) {
+});
+
+var httpsOptions = {
+  pfx: fs.readFileSync('pastrx_cures_cert01.pfx'),
+  passphrase: 'pastrx'
+}
+
+
+//var server = http.createServer(function(req, res) {
+  //uncomment below for HTTPS support
+  var server = https.createServer(httpsOptions, function(req, res) {
+  if(req.url == '/health-check') {
+    return respond(200, '', res);
+  }
+
+  if(!req.headers['x-target']) {
+    console.log('required header "X-Target" not found');
+    return respond(400, 'required header "X-Target" not found', res);
+  }
+
+  var target = req.headers['x-target'];
+  var proxyURL = url.parse(target);
+  var host = proxyURL.host;
+  var protocol = proxyURL.protocol;
+  var agent = (protocol == 'https:' ? https.globalAgent:http.globalAgent);
+  req.url = '';
+
+  console.log('proxy request to: ' + target);
+
+
+  proxy.web(req, res, {
+    target: target,
+    agent: agent,
+    headers: {
+        host: host
+    }
+  });
+});
+
+server.listen(8585, function(){
+  console.log('Server is listening on port 8585');
+});
+// server.listen(443);
